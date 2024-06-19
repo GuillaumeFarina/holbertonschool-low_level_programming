9c9b6f5abb46b7851ad14274dd1f48e3fc72f8db
Wqkz <guillaume.farina@hotmail.com>
2024-06-19 14:23:03 +0200

    Step7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print 'the lowercase alphabet in reverse'
+ * Return: 0
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'z';
+
+	while (n >= 'a')
+		putchar (n);
+	n--;
+
+	putchar ('\n');
+
+	return (0);
+}
