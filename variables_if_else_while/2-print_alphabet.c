5316c9218895dbc2e977091cccc50bbaaaf7c28f
Wqkz <guillaume.farina@hotmail.com>
2024-06-19 10:31:16 +0200

    Step2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - putchar "alphabet in lowerase"
+ * Return: 0
+ */
+int main(void)
+{
+	   char numbers = 'a';
+	   for (numbers; numbers <= 'z';   
+	   putchar(numbers);
+
+	   return 0;
+}
