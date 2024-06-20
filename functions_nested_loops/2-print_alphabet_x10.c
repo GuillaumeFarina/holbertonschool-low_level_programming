c0fc250e0af941cd5e04d2939a17508973426afe
Wqkz <guillaume.farina@hotmail.com>
2024-06-20 15:26:45 +0200

    Step2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 'print_alphabet_x10'
+ * Return 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar ('\n');
+}
