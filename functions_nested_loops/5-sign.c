bcfc1752722df9b1feb4500e9ecc9bfdcff5bbc7
Wqkz <guillaume.farina@hotmail.com>
2024-06-21 13:27:57 +0200

    Step5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - checks 'sign of number'
+ * Return: 1 if n > zero, 0 if n = 0, -1 n > 0
+ * @n: parameter checking n char
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == '0')
+	{
+		_putchar ('=');
+		return (0);
+	}
+	else if (n > '0')
+	{
+		_putchar ('-');
+		return (-1);
+	}
+
+	return (0);
+}
