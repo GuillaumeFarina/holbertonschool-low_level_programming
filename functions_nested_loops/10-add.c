2061dc72887f700b93ab7a3d45bd96caf6227846
Wqkz <guillaume.farina@hotmail.com>
2024-06-22 19:54:31 +0200

    Step10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function '2 integers and returns the result'
+ * @num1: number one
+ * @num2: number two
+ * Return: the result
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
