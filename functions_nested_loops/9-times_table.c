03c60ea36ef4a3fefe3450f71ef77ad88864344c
Wqkz <guillaume.farina@hotmail.com>
2024-06-22 18:21:27 +0200

    Step9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints '9 times table'
+ */
+
+void times_table(void)
+{
+	int num, multiple;
+
+	for (multiple = 0; multiple <= 9; multiple++)
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			if (multiple * num < 10)
+			{
+				_putchar(' ');
+				_putchar((multiple * num) + '0');
+			}
+			else
+		{
+			_putchar(((multiple * num) / 10) + '0');
+			_putchar(((multiple * num) % 10) + '0');
+		}
+			if (num < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	_putchar('\n');
+	}
+}
