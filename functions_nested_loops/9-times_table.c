8a00966e530e8f4cb880ef48af56de40e4f6801c
Wqkz <guillaume.farina@hotmail.com>
2024-06-22 18:51:21 +0200

    Step9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,14 +10,14 @@ void times_table(void)
 
 	for (multiple = 0; multiple <= 9; multiple++)
 	{
-	for (num = 0; num <= 9; num++)
+		for (num = 0; num <= 9; num++)
 		{
-		if (multiple * num < 10)
+			if (multiple * num < 10)
 			{
-			_putchar(' ');
-			_putchar((multiple * num) + '0');
+				_putchar(' ');
+				_putchar((multiple * num) + '0');
 			}
-		else
+			else
 		{
 			_putchar(((multiple * num) / 10) + '0');
 			_putchar(((multiple * num) % 10) + '0');
