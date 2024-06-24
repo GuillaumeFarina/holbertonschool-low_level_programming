a731daf183f79f6d4245a0ee172c33278ee86ebd
Wqkz <guillaume.farina@hotmail.com>
2024-06-24 13:29:16 +0200

    Step2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - write 'function that multiplies two integers'
+ * @a: checker of character a
+ * @b: checker of character b
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
