f4e4cf62961a1844342b40c0e833214fcc38bd44
Wqkz <guillaume.farina@hotmail.com>
2024-06-24 17:13:20 +0200

    Step10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - write 'function that prints a triangle'
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+		{
+		_putchar('\n');
+		}
+	for (a = 1; a <= size; a++)
+	{
+		for (b = size -a; b > 0; b--)
+		{
+		_putchar(' ');
+		}
+
+		for (c = 0; c < a; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
