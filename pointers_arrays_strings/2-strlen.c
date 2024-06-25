b9701b02b9d36e8104ee6aeaf2965e0ce828264f
Wqkz <guillaume.farina@hotmail.com>
2024-06-25 14:32:27 +0200

    Step2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Write 'function that returns the length of a string'
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
