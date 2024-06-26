bd4543c04e5bb17313d7c53ccfc09b2d53b916eb
Wqkz <guillaume.farina@hotmail.com>
2024-06-26 15:45:01 +0200

    Step6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,13 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int l = 0;
 
-	while (str[i] != '\0')
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	while (i < l && str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i += 2;
