14f5fb172876162ddc4462e61ca6855c942f575e
Wqkz <guillaume.farina@hotmail.com>
2024-06-26 18:14:55 +0200

    Step7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,46 +3,21 @@
 /**
  * puts_half - Write 'function that prints half of a string'
  * @str: pointer
- *
+ */
 
 void puts_half(char *str)
-{
-	int l;
-	int length_of_the_string = 10;
-
-	if (length_of_the_string % 2 == 0)
-		l = length_of_the_string / 2;
-
-	if (length_of_the_string != '\0')
-	{
-		length_of_the_string++;
-	}
-
-	while ((length_of_the_string - 1 / 2) && str[l] != '\0')
-	{
-		l++;
-		_putchar(str[l]);
-	}
-	_putchar('\n');
-}
-
-*/
-
-/**
- */
-void puts_half(char *str) 
 {
 	int length = 0;
 	int start;
 	int i;
 
-	while (str[length] != '\0') 
+	while (str[length] != '\0')
 		{
-        	length++;
-    		}
-	if (length % 2 == 0) 
+		length++;
+		}
+	if (length % 2 == 0)
 	{
-		start = length / 2;
+	start = length / 2;
 	}
 	else
 		{
@@ -50,7 +25,7 @@ void puts_half(char *str)
 		}
 	for (i = start; i < length; i++)
 	{
-		_putchar(str[i]);
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 }
