ca897c5e04453e83845966015a45485b15a8076f
Wqkz <guillaume.farina@hotmail.com>
2024-06-28 16:10:53 +0200

    reverse an array of integers

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- *
+ * reverse_array - Write 'function reverses content, an array of integers'
+ * @a: pointer
+ * @n: function
  */
 
 void reverse_array(int *a, int n)
 {
 	int begin = 0;
-	int end = n -1;
+	int end = n - 1;
 	int temporary;
 
 	while (begin < end)
