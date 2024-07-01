be511cfce3610195bece29bace05f0bc2cf47c81
Wqkz <guillaume.farina@hotmail.com>
2024-07-01 13:24:05 +0200

    locates a character in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,25 @@
 #include "main.h"
 
+/**
+ * *_strchr - Write 'function that locates a character in a string'
+ * @s: string
+ * @c: char
+ * Return: s if pointer to the fist occurrence of c, 0 if char not found
+ */
 
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+		if (c == '\0')
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
