459d09a222d65edb65028c4e58f63895c85a41d2
Wqkz <guillaume.farina@hotmail.com>
2024-07-01 15:25:56 +0200

    search string for any bites

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - Write 'function that searches a string for any of a set of byte'
+ * @s: initial segment
+ * @accept: 
+ * Return: s if matches one of bytes in accept, 0 if no byte found
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		++s;
+	}
+	return (0);
+}
