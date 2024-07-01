048089508426d9a00fa838385b7ac96baa515762
Wqkz <guillaume.farina@hotmail.com>
2024-07-01 12:33:09 +0200

    copie memory area

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,10 +2,10 @@
 
 /**
  * *_memset - Write 'function that fills memory with a constant byte'
- * @s: return pointer to memory
- * @b: specify value
+ * @dest: return pointer to memory
+ * @src: specify value
  * @n: memory zone
- * Return: s
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
