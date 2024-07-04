b0c87eff0ce59b056eea187584ccadc5c2b07e6c
Wqkz <guillaume.farina@hotmail.com>
2024-07-04 13:50:27 +0200

    First use argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write 'program that prints its name'
+ * @argc: Count of number argument
+ * @argv: Array of string
+ * Return: 0
+ */
+
+int main(int argc,__attribute__((unused))**argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
