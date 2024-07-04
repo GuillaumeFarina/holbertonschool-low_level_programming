4b3da8461064d5ef432fa35ddc0afcbe457d93e9
Wqkz <guillaume.farina@hotmail.com>
2024-07-04 13:41:03 +0200

    First Argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Write 'program that prints its name'
+ * @argc: Count of number argument
+ * @argv: Array of string
+ * Return: 0
+ */
+
+int main ( __attribute__((unused))int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
