27d94a1c2e5618cdb39c55eb7241d512aa6ef231
Wqkz <guillaume.farina@hotmail.com>
2024-07-08 15:21:07 +0200

    return pointer to new allocated

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - Write 'function that returns a pointer to a newly allocated space in memory'
+ * @str: string to duplicate
+ * Return: NULL if str=NULL and
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int index = 0, len = 0;
+
+	array = malloc(sizeof(char) * (index + 1));
+
+	if (str == NULL && array == NULL)
+		return (NULL);
+
+	for (; str[len]; len++)
+		array[len] = str[len];
+
+	return (array);
+}
