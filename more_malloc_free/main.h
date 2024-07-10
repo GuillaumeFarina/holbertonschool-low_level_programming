c70a08de9c0da5f61ddaf48f2bd6c4439a0f0cf7
Wqkz <guillaume.farina@hotmail.com>
2024-07-10 12:29:55 +0200

    My Library

---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/*------------Function----------------*/
+
+void *malloc_checked(unsigned int b);
+int _putchar(char c);
+
+#endif
