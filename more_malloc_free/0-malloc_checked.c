17ce64e71b9446dcccf10d82eac01690cdec1e1f
Wqkz <guillaume.farina@hotmail.com>
2024-07-10 12:27:39 +0200

    allocated memory

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,14 @@
 
 /**
  * *malloc_checked - Write 'function that allocates memory using malloc'
- * @b:
- * Return: a pointer to the allocated memory
+ * @b: size of memory
+ * Return: pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *a;
+	void *a = malloc(b);
 
-	a = malloc(b);
 	if (a == NULL)
 		exit(98);
 	return (a);
