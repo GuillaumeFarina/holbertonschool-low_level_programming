c21cc56bd6d0255bafb291e0d1b5ffb2c9b24753
Wqkz <guillaume.farina@hotmail.com>
2024-07-11 14:19:55 +0200

    prints a struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,13 +16,11 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 
-/**	if (d->age ==  NULL)
-		printf("Age: (nil)\n");
-	else
+	if (d->age >= 0)
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
-		printf("Owner: %s\n", d->owner);*/
+		printf("Owner: %s\n", d->owner);
 }
