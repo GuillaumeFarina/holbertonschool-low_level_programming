56400a35c1f065adac7bc2dde07f1f00ee0ea2f5
Wqkz <guillaume.farina@hotmail.com>
2024-07-11 15:02:24 +0200

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,40 +11,34 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nameLen, ownerLen;
-	int index = 0;
+	int nameLen, ownerLen, index;
 	dog_t *d1;
 
 	d1 = (dog_t *)malloc(sizeof(dog_t));
-
 	if (d1 == NULL)
 		return (NULL);
-
 	nameLen = ownerLen = 0;
-
 	while (name[nameLen++])
 		;
 	while (owner[ownerLen++])
 		;
 	d1->name = malloc(nameLen * sizeof(d1->name));
-
 	if (d1->name == NULL)
 	{
 		free(d1);
 		return (NULL);
 	}
-	for (; index <= nameLen; index++)
+	for (index = 0; index <= nameLen; index++)
 		d1->name[index] = name[index];
 	d1->age = age;
 	d1->owner = malloc(ownerLen * sizeof(d1->owner));
-
 	if (d1->owner == NULL)
 	{
 		free(d1->name);
 		free(d1);
 		return (NULL);
 	}
-	for (; index <= ownerLen; index++)
+	for (index = 0; index <= ownerLen; index++)
 		d1->owner[index] = owner[index];
 	return (d1);
 }
