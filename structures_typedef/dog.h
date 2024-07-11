088ead26b5a769e0db9a9ce5b7b4f3f82dd2b3ac
Wqkz <guillaume.farina@hotmail.com>
2024-07-11 11:30:20 +0200

    define new type

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ *struct dog - dog information
+ *@name: name of the dog
+ *@owner: owner of the dog
+ *@age: age of the dog
+ */
+
 struct dog
 {
 	char *name;
@@ -12,4 +19,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* DOG.H*/
