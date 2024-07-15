6b572878bb351bfcaf257f9efe24e9cce19b8e14
Wqkz <guillaume.farina@hotmail.com>
2024-07-15 23:36:32 +0200

    Details function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "function_pointers.h"
+
+/**
+ * op_add - Write 'function additional two variable'
+ * @a: first variable
+ * @b: second variable
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Write 'function subtraction two variable'
+ * @a: first variable
+ * @b: second variable
+ * Return: subtraction of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Write 'function multiply two variable'
+ * @a: first variable
+ * @b: second variable
+ * Return: multiply of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Write 'function divide two variable'
+ * @a: first variable
+ * @b: second variable
+ * Return: divide of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Write 'function modulo two variable'
+ * @a: first variable
+ * @b: second variable
+ * Return: modulo of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
