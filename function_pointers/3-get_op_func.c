fddae3870af51ee9773363e09ef05e087858e4bc
Wqkz <guillaume.farina@hotmail.com>
2024-07-16 01:02:58 +0200

    correct function to perform the operation

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - Exec 'the correct function'
@@ -8,4 +8,22 @@
 
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
 
+	while (ops[i].op != NULL)
+	{
+		if (*s == *ops[i].op)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
