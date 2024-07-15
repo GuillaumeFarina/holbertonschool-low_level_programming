8f5a0ebdb7e51c19c685a4890e26df674b8cde6b
Wqkz <guillaume.farina@hotmail.com>
2024-07-15 16:48:29 +0200

    searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,9 +3,9 @@
 
 /**
  * int_index - Write 'function that searches for an integer'
- * @array: array to check
+ * @array: array of integers
  * @size: number of elements in the array
- * @cmp: pointer to the function to be used to compare values
+ * @cmp: pointer to the function for compare values
  * Return: 0 if element marches, -1 if no element match
  * or if size <= 0
  */
@@ -14,12 +14,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]))
 			return (i);
 	}
 	return (-1);
