2f81e65ff8ecd7c585ee7ebfbdbaed1f30bebbd7
Wqkz <guillaume.farina@hotmail.com>
2024-07-17 13:58:26 +0200

    add struct def and more prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,8 +4,26 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct format_specifier - struct to map specifiers to functions.
+ * @specifier: format specifier character.
+ * @print_func: pointer to the function that handles printing for specifier.
+ * Despcription: associate format specifiers with corresponding print function.
+ */
+
+typedef struct format_specifier
+{
+	char specifier;
+	void (*print_func)(va_list ap);
+} format_specifier_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(va_list ap);
+void print_int(va_list ap);
+void print_float(va_list ap);
+void print_string(va_list ap)
 
 #endif
