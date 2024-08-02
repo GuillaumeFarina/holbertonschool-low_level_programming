53c8daa22896e405fd74f97425c8f541a0010b44
Wqkz <guillaume.farina@hotmail.com>
2024-08-02 15:23:17 +0200

    add library inside my main.h

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,41 +10,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	ssize_t bytes_read, bytes_written;
+	int fd, read_2, write_2;
 	char *buffer;
 
-	if (filename == NULL)
+	buffer = malloc(letters * sizeof(char));
+
+	if (!buffer && !filename)
 	{
 		return (0);
 	}
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-	{
-		return (0);
-	}
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(file_descriptor);
-		return (0);
-	}
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
+	fd = open(filename, O_RDONLY);
+	read_2 = read(fd, buffer, letters);
+	write_2 = write(STDOUT_FILENO, buffer, read_2);
+
+	if (fd == -1 || read_2 == -1 || write_2 == -1 || write_2 != read_2)
 	{
 		free(buffer);
-		close(file_descriptor);
-		return (0);
-	}
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(file_descriptor);
 		return (0);
 	}
 	free(buffer);
-	close(file_descriptor);
-	return (bytes_written);
+	close(fd);
+	return (write_2);
 }
