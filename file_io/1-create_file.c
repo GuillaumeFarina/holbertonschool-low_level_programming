f62467762979f45890af4786b0467924006bd258
Wqkz <guillaume.farina@hotmail.com>
2024-08-02 17:36:48 +0200

    finally it's gonna be work

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *create_file - Create 'function that creates a file'
- *@filename: name of the file
- *@text_content: NULL terminated string to write to the file
- *Return: 1 if success, 0 if fail
+ * create_file - Create 'function that creates a file'
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 if success, 0 if fail
  */
 
 int create_file(const char *filename, char *text_content)
@@ -12,10 +12,16 @@ int create_file(const char *filename, char *text_content)
 	int fd, len = 0;
 
 	if (!filename)
+	{
 		return (-1);
+	}
+
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
+	{
 		return (-1);
+	}
 	if (text_content != NULL)
 	{
 		while (text_content[len])
