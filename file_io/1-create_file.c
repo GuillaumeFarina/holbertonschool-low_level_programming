fb1a57a1db515f7779b931db7975eded654bd575
Guillaume Farina <guillaume.farina@hotmail.com>
2024-08-02 16:46:29 +0200

    fix code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,15 +13,10 @@ int	create_file(const char *filename, char *text_content)
     ssize_t	write_2;
 
 	if (!filename)
-	{
 		return (-1);
-	}
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,  0600);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
 		while (text_content[len])
