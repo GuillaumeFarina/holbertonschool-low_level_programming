01e21ece2f2fab7b94bdc5296712441cd0a46caf
Wqkz <guillaume.farina@hotmail.com>
2024-08-02 14:52:37 +0200

    try to fix

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,26 +10,34 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, write_2, read_2;
+	int fd;
+	ssize_t n_read, n_written;
 	char *buffer;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
-	buffer = malloc(letters * sizeof(char));
-
-	if (!buffer)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
 		return (0);
 	}
-
-	fd = open(filename, O_RDONLY);
-	read_2 = read(fd, buffer, letters);
-	write_2 = write(STDOUT_FILENO, buffer, read_2);
-
-	if (fd == -1 || write_2 == -1 || write_2 != read_2 || read_2 == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	n_written = write(STDOUT_FILENO, buffer, n_read);
+	if (n_written == -1 || n_written != n_read)
 	{
 		free(buffer);
 		close(fd);
@@ -37,5 +45,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(buffer);
 	close(fd);
-	return (write_2);
+	return (n_written);
 }
